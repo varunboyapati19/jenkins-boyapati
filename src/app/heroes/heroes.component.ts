a0adaa1ac44cbed070ca755199c8f6c7370f100f
varunboyapati19 <varunboyapati19@gmail.com>
2024-08-08 15:25:46 +0530

    hello Varun

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,32 @@
+import {Component} from '@angular/core';
+import {
+  NgIf,
+  NgFor,
+  UpperCasePipe,
+} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
+
+@Component({
+  standalone: true,
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  imports: [
+    FormsModule,
+    NgIf,
+    NgFor,
+    UpperCasePipe,
+  ],
+})
+
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
